b5780a6a961978669eb25164f0f4c70c77939202
Douglas Fernandes <br.com.age@gmail.com>
2017-12-17 16:42:28 -0300

    [AGE - Aplicação de Gestão Estudantil v1.0] - Douglas Fernandes: Atualização da estrutura da base de dados e da própria ase de dados

--- database/agedb.sql
+++ database/agedb.sql
@@ -18,6 +18,107 @@ USE `agedb`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `address` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address` varchar(150) NOT NULL,
+  `district` varchar(55) NOT NULL,
+  `city` varchar(65) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `postal_code` varchar(15) NOT NULL,
+  `number` varchar(45) DEFAULT NULL,
+  `complement` varchar(150) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `createdBy` bigint(20) NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `updatedBy` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_address_created_by_idx` (`createdBy`),
+  KEY `fk_address_updated_by_idx` (`updatedBy`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grade`
+--
+
+DROP TABLE IF EXISTS `grade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grade` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `matter` bigint(20) NOT NULL,
+  `position` bigint(20) NOT NULL,
+  `student` bigint(20) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `createdBy` bigint(20) NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `updatedBy` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_grade_matter_idx` (`matter`),
+  KEY `fk_grade_position_idx` (`position`),
+  KEY `fk_grade_student_idx` (`student`),
+  KEY `fk_grade_created_by_idx` (`createdBy`),
+  KEY `fk_grade_updated_by_idx` (`updatedBy`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grade`
+--
+
+LOCK TABLES `grade` WRITE;
+/*!40000 ALTER TABLE `grade` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `matter`
+--
+
+DROP TABLE IF EXISTS `matter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `matter` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `teacher` bigint(20) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `createdBy` bigint(20) NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `updatedBy` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  KEY `fk_matter_teacher_idx` (`teacher`),
+  KEY `fk_matter_created_by_idx` (`createdBy`),
+  KEY `fk_matter_updated_by_idx` (`updatedBy`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `matter`
+--
+
+LOCK TABLES `matter` WRITE;
+/*!40000 ALTER TABLE `matter` DISABLE KEYS */;
+/*!40000 ALTER TABLE `matter` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `permission`
 --
 
@@ -41,6 +142,38 @@ LOCK TABLES `permission` WRITE;
 /*!40000 ALTER TABLE `permission` DISABLE KEYS */;
 INSERT INTO `permission` VALUES (1,'Administrador','Administrador'),(2,'Aluno','Aluno'),(3,'Professor','Professor');
 /*!40000 ALTER TABLE `permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `position`
+--
+
+DROP TABLE IF EXISTS `position`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `position` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `position` int(11) NOT NULL,
+  `description` varchar(60) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `createdBy` bigint(20) NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `updatedBy` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `position_UNIQUE` (`position`),
+  UNIQUE KEY `description_UNIQUE` (`description`),
+  KEY `fk_position_created_by_idx` (`createdBy`),
+  KEY `fk_position_updated_by_idx` (`updatedBy`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `position`
+--
+
+LOCK TABLES `position` WRITE;
+/*!40000 ALTER TABLE `position` DISABLE KEYS */;
+/*!40000 ALTER TABLE `position` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -96,4 +229,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-16 20:22:57
+-- Dump completed on 2017-12-17 16:41:11
